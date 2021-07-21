1f3e6d906d82fd2cadd5ee4793fd165168352e44
macauguste <mckernauguste@gmail.com>
2021-07-20 21:24:40 -0400

    dom manipulation

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -22,9 +22,14 @@ var scores, roundScore, activePlayer, gamePlaying;
 
 scores = [0,0]
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
 
+document.querySelector('#current-' + activePlayer).textContent = dice;
 
+var x = document.querySelector('#score-1').textContent;
+console.log(x);
 
+document.querySelector('.dice').style.display = 'none';

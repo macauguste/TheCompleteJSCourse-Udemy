df2c256ce4fa0a6761ba06028cba662994c8e6c8
macauguste <mckernauguste@gmail.com>
2021-07-21 22:05:31 -0400

    first DOM access and mannipulation

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -24,12 +24,29 @@ scores = [0,0]
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//var x = document.querySelector('#score-1').textContent;
+//console.log(x);
 
-var x = document.querySelector('#score-1').textContent;
-console.log(x);
 
-document.querySelector('.dice').style.display = 'none';
+document.getElementById('dice-1').style.display = 'none';
+document.getElementById('dice-2').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1. Random number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. DIsplay the result 
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display ='block';  
+    diceDOM.src = 'dice-'+ dice + '.png';
+
+    //3. Update the round score IF the rolled number was NOT a 1
+
+});

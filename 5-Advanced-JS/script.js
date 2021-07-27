cab840f04026f60cdd47bbec517c56c3a78a66c8
macauguste <mckernauguste@gmail.com>
2021-07-27 15:53:34 -0400

    First classs functions returning functions

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -124,5 +124,33 @@ console.log(rates)
 console.log(ages);
 */
 
+///Functions returning functions
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name){
+          console.log(name + ', can you please explain what UX design is?');
+        }
+    }else if (job === 'teacher'){
+        return function(name){
+          console.log('What subject do you teach, ' + name + '?');
+        }
+    }else{
+        return function(name){
+          console.log('Hello ' + name + ', what do you do?')
+        }
+    }
+}
 
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
 
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark')
+*/
+
+//

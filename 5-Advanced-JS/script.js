a2398b332fdfffb15be130297e30a0709e59c2b3
macauguste <mckernauguste@gmail.com>
2021-07-27 18:17:15 -0400

    Coding Challenge 7 advanced

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -281,5 +281,83 @@ questions[randomQuestion].checkAnswer(answer);
 })();
 */
 
+//Coding Challenge 7 advanced
+/*
+(function() {
+function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+}
+ 
+Question.prototype.displayQuestion = function(){
+  console.log(this.question);
+
+  for(var i = 0; i < this.answers.length; i++)
+  {
+      console.log(i + ': ' + this.answers[i]);
+  }
+}
+
+Question.prototype.checkAnswer = function(ans, callback) {
+  var sc;
+
+  if (ans === this.correct) {
+      console.log('Correct answer!');
+
+      sc = callback(true);
+  }else {
+      console.log('Wrong Answer!');
+
+      sc = callback(false);
+
+  }
+
+  this.displayScore(sc);
+}
+
+Question.prototype.displayScore = function(score)
+{
+    console.log('Your current score is: ' + score);
+    console.log('----------------------------------');
+}
+
+  var q1 = new Question('Is Messi the best player in the world?', ['Yes', 'No'], 0);
+
+  var q2 = new Question('Is Lionel Messi the greatest player of all time?', ['Yes', 'No'], 0);
+
+  var q3 = new Question('Is Coding something you would like to learn?', ['Yes', 'No'], 0);  
+
+  var questions = [q1, q2, q3];
+  
+  function score(){
+      var sc = 0;
+      return function(correct){
+        if (correct)
+        sc++;
+        return sc;
+      }
+  }
+
+  var keepScore = score();
+
+  function nextQuestion() {
+
+  var randomQuestion = Math.floor(Math.random() * questions.length);
+
+  questions[randomQuestion].displayQuestion();
+
+  var answer = (prompt('Please select the correct answer.'));
 
 
+  if (answer !== 'exit') {
+    questions[randomQuestion].checkAnswer(parseInt(answer),keepScore);
+
+    nextQuestion();
+  }
+}
+nextQuestion();
+
+})();
+
+*/
